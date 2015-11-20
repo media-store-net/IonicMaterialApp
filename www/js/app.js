511acc564323a7f2b1fb6ded66194598730af851
Artur <info@pcservice-voll.de>
2015-11-20 23:57:10 +0100

    Templates hinzugefuegt

--- www/js/app.js
+++ www/js/app.js
@@ -1,19 +1,41 @@
-// Ionic Starter App
+(function () {
+    var app = angular.module('webapp', ['ionic']);
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+    //APP Konfiguration
+    app.config(function ($stateProvider, $urlRouterProvider) {
+        // Templates //
+        $stateProvider.state('home', {
+            url: '/home',
+            templateURL: '/templates/home.html'
+        });
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+        $stateProvider.state('listen', {
+            url: '/listen',
+            templateURL: 'templates/listen.html'
+        });
+
+        $stateProvider.state('singleList', {
+            url: '/list',
+            templateURL: 'templates/single-list.html'
+        });
+
+        $stateProvider.state('login', {
+            url: '/login',
+            templateURL: 'templates/login.html'
+        });
+
+        $urlRouterProvider.otherwise('/home');
+
+    });
+
+    app.run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if (window.StatusBar) {
+                StatusBar.styleDefault();
+            }
+        });
+    });
+}());
